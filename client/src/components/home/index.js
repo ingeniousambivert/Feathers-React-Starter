088fc19dfa9198d485ccdc282f65d7eed38ef954
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-10-10 16:18:20 +0530

    Update Client

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Divider, Tabs, Row, Col, message, Badge, notification } from "antd";
-import { ProjectOutlined, UserOutlined, BellOutlined } from "@ant-design/icons";
+import { Divider, Row, Col, message, notification } from "antd";
 import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
 import { loadUserThunk, removeUserAction, selectUser, selectError } from "@slices/user";
 import { selectUserID, signOutUserThunk } from "@slices/auth";
 
 import { Spinner, RandomGreet, primaryColor } from "@utils";
 
-import Projects from "./sections/projects";
-import Notices from "./sections/notices";
 import User from "./sections/user";
-
-const { TabPane } = Tabs;
 
 function HomeComponent() {
 	const dispatch = useDispatch();
@@ -20,8 +14,6 @@ function HomeComponent() {
 	const userID = useSelector(selectUserID);
 	const user = useSelector(selectUser);
 	const [greet, setGreet] = useState();
-	let [activeKey, setActiveKey] = useState("1");
-	let location = useLocation();
 
 	const loadUser = async (userID, error) => {
 		await dispatch(loadUserThunk(userID));
@@ -45,25 +37,6 @@ function HomeComponent() {
 		});
 	};
 
-	const onTabChange = (key) => {
-		setActiveKey(key);
-	};
-	const changeTabs = (state) => {
-		if (state) {
-			state.to === "userSettings" && setActiveKey("3");
-			state.to === "userNotices" && setActiveKey("2");
-			state.to === "projectsList" && setActiveKey("1");
-		} else {
-			setActiveKey("1");
-		}
-	};
-	const tabStyle = { height: 475 };
-	const tabpaneStyle = {
-		height: 475,
-		overflowY: "scroll"
-	};
-	const badgeStyle = { backgroundColor: primaryColor };
-
 	const renderUserName = (user) => (
 		<div>
 			<b>{greet}, </b>
@@ -74,7 +47,6 @@ function HomeComponent() {
 	useEffect(() => {
 		loadUser(userID, error);
 		greetUser();
-		changeTabs(location.state);
 	}, [error]);
 
 	return (
@@ -101,58 +73,7 @@ function HomeComponent() {
 						<Row gutter={[16, 48]} justify="space-around" align="middle">
 							<Col xs={23} sm={21} md={20} lg={18} xl={18}>
 								<div>
-									<Tabs
-										centered
-										type="card"
-										size="large"
-										onChange={onTabChange}
-										activeKey={activeKey}
-										tabPosition="left"
-										style={tabStyle}>
-										<TabPane
-											tab={
-												<span>
-													{" "}
-													<ProjectOutlined /> Projects{" "}
-												</span>
-											}
-											key="1">
-											<div className="profileTab" style={tabpaneStyle}>
-												<Projects userinfo={user} />
-											</div>
-										</TabPane>
-
-										<TabPane
-											tab={
-												<span>
-													<BellOutlined /> Notices
-													<Badge
-														count={0}
-														style={badgeStyle}
-														offset={[5, 0]}
-														showZero
-													/>{" "}
-												</span>
-											}
-											key="2">
-											{" "}
-											<div className="profileTab" style={tabpaneStyle}>
-												<Notices />
-											</div>
-										</TabPane>
-
-										<TabPane
-											tab={
-												<span>
-													<UserOutlined /> Account
-												</span>
-											}
-											key="3">
-											<div className="profileTab" style={tabpaneStyle}>
-												<User userinfo={user} />
-											</div>
-										</TabPane>
-									</Tabs>
+									<User userinfo={user} />
 								</div>
 							</Col>
 						</Row>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,18 +1,14 @@
 import { configureStore, getDefaultMiddleware, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-//import logger from "redux-logger";
+import logger from "redux-logger";
 
 import authReducer from "@slices/auth/";
 import userReducer from "@slices/user";
-import projectReducer from "@slices/project";
-import projectsReducer from "@slices/projects";
 
 const rootReducers = combineReducers({
 	auth: authReducer,
-	user: userReducer,
-	project: projectReducer,
-	projects: projectsReducer
+	user: userReducer
 });
 
 const persistConfig = {
@@ -26,8 +22,8 @@ const middleware = [
 		serializableCheck: {
 			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
 		}
-	})
-	//logger,
+	}),
+	logger
 ];
 
 const persistedReducer = persistReducer(persistConfig, rootReducers);
