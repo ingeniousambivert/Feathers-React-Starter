d583fcb46b1f6708a6cbde916680a48f5907b626
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-10-11 20:35:13 +0530

    Update Auth service

--- server/src/auth/authentication.js
+++ server/src/auth/authentication.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const hooks = require("./authentication.hooks");
 
 const {
@@ -8,60 +7,13 @@ const {
 const { LocalStrategy } = require("@feathersjs/authentication-local");
 const {
   expressOauth,
-  OAuthStrategy
 } = require("@feathersjs/authentication-oauth");
-
-class GoogleStrategy extends OAuthStrategy {
-  async getEntityData(profile) {
-    const baseData = await super.getEntityData(profile);
-    console.log(profile);
-    return {
-      ...baseData,
-      picture: profile.picture,
-      name: profile.name,
-      email: profile.email
-    };
-  }
-}
-
-class FacebookStrategy extends OAuthStrategy {
-  async getProfile(authResult) {
-    // This is the oAuth access token that can be used
-    // for Facebook API requests as the Bearer token
-    const accessToken = authResult.access_token;
-
-    const { data } = await axios.get("https://graph.facebook.com/me", {
-      headers: {
-        authorization: `Bearer ${accessToken}`
-      },
-      params: {
-        // There are
-        fields: "id,name,email,picture"
-      }
-    });
-
-    return data;
-  }
-  async getEntityData(profile) {
-    // `profile` is the data returned by getProfile
-    const baseData = await super.getEntityData(profile);
-
-    return {
-      ...baseData,
-      picture: profile.profile_pic,
-      name: profile.name,
-      email: profile.email
-    };
-  }
-}
 
 module.exports = (app) => {
   const authentication = new AuthenticationService(app);
 
   authentication.register("jwt", new JWTStrategy());
   authentication.register("local", new LocalStrategy());
-  authentication.register("facebook", new FacebookStrategy());
-  authentication.register("google", new GoogleStrategy());
 
   app.use("/authentication", authentication);
   app.configure(expressOauth());
