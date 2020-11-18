6b378acbe2778afec3ba3f974492b0af2960e630
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-11-19 00:20:42 +0530

    Updated routes: add forgot/reset password route

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,6 +5,8 @@ import Signin from "@pages/signin";
 import Signup from "@pages/signup";
 import Home from "@pages/home";
 import Verify from "@pages/verify";
+import ForgotPassword from "@pages/forgotPassword";
+import ResetPassword from "@pages/resetPassword";
 
 import ProtectedRoute from "./protectedRoute";
 import PublicRoute from "./publicRoute";
@@ -18,9 +20,19 @@ const Routes = () => {
 				<PublicRoute exact path="/signup" component={(props) => <Signup {...props} />} />
 				<PublicRoute exact path="/signin" component={(props) => <Signin {...props} />} />
 
-				<Route exact path="/verify" component={(props) => <Verify {...props} />} />
-
 				<ProtectedRoute exact path="/home" component={(props) => <Home {...props} />} />
+
+				<Route exact path="/verify" component={(props) => <Verify {...props} />} />
+				<Route
+					exact
+					path="/forgot-password"
+					component={(props) => <ForgotPassword {...props} />}
+				/>
+				<Route
+					exact
+					path="/reset-password"
+					component={(props) => <ResetPassword {...props} />}
+				/>
 
 				<Route component={NotFound} />
 			</Switch>
