e404de2ce6be53ed2e098126347adf294f9c351c
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-11-19 14:59:33 +0530

    Updated utils

--- client/src/utils/greet.js
+++ client/src/utils/greet.js
@@ -5,9 +5,8 @@ function RandomGreet() {
 	greet[2] = "Hi";
 	greet[3] = "Hey";
 	greet[4] = "Welcome";
-	greet[5] = "Hello there";
+	greet[5] = "Hey there";
 	greet[6] = "Hi there";
-	greet[7] = "Hey there";
 
 	let genGreet = Math.floor(Math.random() * greet.length);
 	if (genGreet === 0) genGreet = 1;
