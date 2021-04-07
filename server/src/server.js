5e34c201c57e2b27bd2b10ec40a676b1bc60d102
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-04-07 12:49:27 +0530

    add timestamp

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-console */
 const logger = require("./utils/logger");
+const timeStamp = require("./utils");
 const app = require("./app");
 const port = app.get("port");
 const server = app.listen(port);
+
+
 
 process.on("unhandledRejection", (reason, p) =>
   logger.error("Unhandled Rejection at: Promise ", p, reason)
 );
 
 server.on("listening", () =>
-  logger.info(
-		"server running on http://%s:%d",
+  logger.info(`${timeStamp}: server running on http://%s:%d`,
 		app.get("host"), port
   )
 );
