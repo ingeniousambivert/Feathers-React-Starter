a4c51425941c7ef0f8d3d280ada43a0461d14262
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-04-17 00:21:34 +0530

    add admin route

---
+++ web/src/routes/adminRoute/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { selectIsAdmin } from "@slices/user";
+
+import Home from "@pages/home";
+
+const AdminRoute = (props) => {
+	const { component: Component, ...rest } = props;
+	let isAdmin = useSelector(selectIsAdmin);
+
+	return (
+		<Route
+			{...rest}
+			render={(props) => {
+				if (isAdmin) {
+					return <Component {...props} />;
+				} else {
+					return <Home />;
+				}
+			}}
+		/>
+	);
+};
+
+AdminRoute.propTypes = {
+	component: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.element,
+		PropTypes.string,
+		PropTypes.func
+	]).isRequired
+};
+
+export default AdminRoute;
--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -5,10 +5,12 @@ import Landing from "@pages/landing";
 import Signin from "@pages/signin";
 import Signup from "@pages/signup";
 import Home from "@pages/home";
+import Admin from "@pages/admin";
 import Verify from "@pages/verify";
 import ForgotPassword from "@pages/password/forgot";
 import ResetPassword from "@pages/password/reset";
 
+import AdminRoute from "./adminRoute";
 import ProtectedRoute from "./protectedRoute";
 import PublicRoute from "./publicRoute";
 
@@ -23,6 +25,7 @@ const Routes = () => {
 				<PublicRoute exact path="/signin" component={(props) => <Signin {...props} />} />
 
 				<ProtectedRoute exact path="/home" component={(props) => <Home {...props} />} />
+				<AdminRoute exact path="/admin" component={(props) => <Admin {...props} />} />
 
 				<Route exact path="/verify" component={(props) => <Verify {...props} />} />
 				<Route
